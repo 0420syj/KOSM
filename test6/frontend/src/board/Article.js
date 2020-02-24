c4b11e5da93d1547e5dbf499a068d1a3a7cb97c6
Wan Sim <0420syj@naver.com>
2020-02-25 01:15:23 +0900

    게시판 읽기 기능추가
    - 게시판 읽기 가능
    - 게시판 목록 localStorage 문제 수정중

--- test6/frontend/src/board/Article.js
+++ test6/frontend/src/board/Article.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
-import {getArticle} from '../util/APIUtils'
+import { getArticle } from '../util/APIUtils'
 
-const Article = ({match}) => {
+class Article extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: "순번",
+            title: "제목",
+            content: "내용",
+            author: "작성자",
+            status: "상태",
+            date: "날짜",
+        }
+    }
 
-    console.log(match.params.id)
+    componentDidMount() {
+        getArticle(this.props.match.params.id)
+            .then(res => {
+                this.setState({
+                    id: res.id,
+                    title: res.title,
+                    content : res.content,
+                    author : res.author,
+                    status : res.status,
+                    date : res.createdDate,
+                })
+            })
+    }
 
-    const data = new Object();
+    render() {
+        const { title, content, author, status, date } = this.state;
 
-    getArticle(match.params.id).then(res => {
-        localStorage.removeItem("article");
-        data.id = res.id;
-        data.title = res.title;
-        data.content = res.content;
-        data.author = res.author;
-        data.status = res.status;
-        data.date = res.createdDate;
-        localStorage.article = JSON.stringify(data);
-    })
-
-    return (
-        <div>
-            <h2>제목 : {JSON.parse(localStorage.article).title}</h2>
-            <h2>내용 : {JSON.parse(localStorage.article).content}</h2>
-        </div>
-    );
+        return (
+            <div>
+                <h2>제목 : {title}</h2>
+                <h2>내용 : {content}</h2>
+                <h4>작성자 : {author}</h4>
+                <h4>상태 : {status}</h4>
+                <h4>날짜 : {date}</h4>
+            </div>
+        );
+    }
 }
-
 export default Article;
---
+++ test6/frontend/src/board/Board copy.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
+import './Board.scss';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import renderEmpty from 'antd/lib/config-provider/renderEmpty';
+import { getBoards } from '../util/APIUtils'
+import BeforeMenu from '../menu/before/BeforeMenu';
+import AfterMenu from '../menu/after/AfterMenu';
+import { Button } from 'reactstrap';
+
+// localStorage 초기화 영역 시작 
+
+const data = [];
+
+getBoards()
+    .then(response => {
+        localStorage.removeItem("articles"); // 초기화
+    response.map(res => {
+        data.push({
+            id: res.id,
+            title: res.title,
+            status: '처리중', // 임시 데이터
+            author: res.author,
+            time: res.createdDate
+        })
+    })
+    // console.log(data);
+        localStorage.articles = JSON.stringify(data); // localStorage에 저장
+    });
+
+// localStorage 초기화 끝 
+
+const { SearchBar } = Search;
+
+const columns = [
+    {
+        dataField: 'id',
+        text: '번호',
+        type: 'number',
+        headerStyle: () => {
+            return { width: '60px' };
+        },
+    },
+    {
+        dataField: 'title',
+        text: '제목',
+        headerStyle: () => {
+            return { width: '120px' };
+        },
+    },
+    {
+        dataField: 'status',
+        text: '상태',
+        headerStyle: () => {
+            return { width: '60px' };
+        },
+    },
+    {
+        dataField: 'author',
+        text: '글쓴이',
+        headerStyle: () => {
+            return { width: '120px' };
+        },
+    },
+    {
+        dataField: 'time',
+        text: '등록일',
+        type: 'date',
+        formatter: (cell) => {
+            let dateObj = cell;
+            if (typeof cell !== 'object') {
+              dateObj = new Date(cell);
+            }
+            return `${dateObj.getFullYear()}/${('0' + (dateObj.getMonth() + 1)).slice(-2)}/${('0' + dateObj.getDate()).slice(-2)}`;
+          },
+        headerStyle: () => {
+            return { width: '100px' };
+        },
+    }
+];
+  
+const Board = () => {
+    return (
+        <div>
+            <div>
+                {
+                    localStorage.getItem('isLogin') === 'false' ?
+                    <BeforeMenu/>:
+                    <AfterMenu/>
+                }
+            </div>
+            <div className='boardScreen'>
+                <ToolkitProvider
+                    keyField="id"
+                    data={ JSON.parse(localStorage.articles) } // localStorgae값 불러오기
+                    columns={ columns }
+                    search>
+                    {
+                        props => (
+                            <div>
+                                <h3>게시판</h3>
+                                <SearchBar
+                                    { ...props.searchProps }
+                                    className="custome-search-field"
+                                    placeholder="검색"/>
+                                <hr />
+                                <BootstrapTable
+                                    striped
+                                    hover
+                                    condensed
+                                    { ...props.baseProps }
+                                    pagination={ paginationFactory() } 
+                                    noDataIndication="내용이 없습니다"
+                                />
+                                {
+                                localStorage.getItem('isLogin') === 'false' ? '' :
+                                <Link to='/write'>
+                                    <Button>글쓰기</Button>
+                                </Link>}
+                            </div>
+                        )
+                    }
+                </ToolkitProvider>
+            </div>
+        </div>
+    )
+};
+
+export default Board;
