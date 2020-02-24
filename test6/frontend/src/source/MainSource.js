61d9c450c95dcbb035ca49316cf6069c1d1570ad
taewankang <king6096211@naver.com>
2020-02-25 03:06:12 +0900

    즐겨찾기 toggle기능
    로그인 후 즐겨찾기 버튼을 누르면 alert가 뜨고 색이 바뀌도록 구현

--- test6/frontend/src/source/MainSource.js
+++ test6/frontend/src/source/MainSource.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {MdStar, MdStarBorder} from 'react-icons/md'
+import {IconContext} from 'react-icons';
 const MainSource = (props) => {
+    const [isFavorite, setIsFavorite] = useState(false);
+    // async function asyncOrder() {
+    //     return await Promise.resolve(
+    //         setIsFavorite(!isFavorite),
+    //         console.log(isFavorite)
+    //     )
+    // }
+    // async function favoriteClick() {
+    //     asyncOrder()
+    //     .then(() => {
+    //         console.log('success');
+    //         console.log(isFavorite);
+    //     }
+    //     )
+    //     .catch(e => {
+    //         console.log(e);
+    //     })
+    // }
+
+    const favoriteClick = () => {
+        setIsFavorite(!isFavorite);
+    }
+    useEffect(() => {
+        if(isFavorite === true)
+            alert('즐겨찾기가 추가되었습니다.');
+    }, [isFavorite])
+
     return ( 
         <div>
-            <h2>{props.name}</h2>
+            <div style={{display: 'flex'}}>
+                {
+                    localStorage.getItem('isLogin') === 'true' ?
+                    <div>
+                        {
+                            isFavorite === true ?
+                            <IconContext.Provider
+                                value={{color: 'yellow'}}>
+                                <MdStar onClick = {favoriteClick}/> 
+                            </IconContext.Provider> :
+                            <MdStarBorder onClick = {favoriteClick}/>
+                        }
+                    </div>:
+                    null
+                }
+                <h2>{props.name}</h2>
+            </div>
             
         </div>
     )
