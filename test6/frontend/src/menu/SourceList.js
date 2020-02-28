f73c685a7d980eb1bc9ff140bf061f3747c7c8eb
Joyful <0420syj@naver.com>
2020-02-28 11:16:20 +0900

    소스리스트 완료
    - Zeplin 사양 반영(일부는 다름)
    - 스크롤바 디자인 수정
    - 소스리스트 영역 height 재계산 필요할 수 있음

--- test6/frontend/src/menu/SourceList.js
+++ test6/frontend/src/menu/SourceList.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import OpenSourceData from '../data/OpenSourceData';
 import './SourceList.scss';
-import {WIDTH, HEIGHT} from '../constants';
+import { WIDTH, HEIGHT } from '../constants';
 const SourceList = () => {
     const [Data, setData] = useState([]);
     const onChange = (e) => {
@@ -10,70 +10,91 @@ const SourceList = () => {
             return item.value.indexOf(e.target.value) !== -1
         }));
     }
-    return ( 
-        <div 
+    return (
+        <div
             style={{
-                width: `444px`, 
-                background: '#414141', 
-                paddingLeft: `${WIDTH * 48}px`}}>
-            <div>
-                <div
-                    style={{
-                        color: '#e4e4e4',
-                        width: `${WIDTH * 121}px`,
-                        height: '41px',
-                        fontFamily: 'NotoSans',
-                        fontSize:'30px',
-                        fontWeight: 'bold',
-                        fontStretch: 'normal',
-                        fontStyle: 'normal',
-                        lineHeight: '1.37',
-                        letterSpacing: 'normal',
-                        textAlign: 'left'}}>
-                    Projects
-                </div>
-                <div>
-                    <label 
-                        style={{
-                            justifyContent:'center', 
-                            display: 'flex'}}>
-                        <input 
-                            type='text' 
-                            placeholder='search...' 
-                            onChange={onChange}/>
-                    </label>
-                </div>
-            </div>
-            <ul 
+                height: '915px',
+                width: `444px`,
+                background: '#414141',
+                paddingLeft: `${WIDTH * 48}px`,
+                paddingTop: `${HEIGHT * 38}px`,
+            }}>
+            <div
                 style={{
-                    overflowY: 'scroll', 
-                    height: `990px`, 
-                    maxHeight: `990px`, 
-                    listStyleType: 'none',
-                    margin:'0px 0px 0px 0px',
-                    padding: '0px 0px 0px 0px'}}>
-                {
-                    Data.length === 0 ?
-                    OpenSourceData.map(item => {
-                        return  <li key={item.id}>
-                            <Link to={`/source/${item.value}`}
-                                className='sourceList'>
-                                {item.value}
-                            </Link>
-                        </li>
-                    }):
-                    
-                    Data.map(item => {
-                        return <li key={item.id} >
-                            <Link 
-                                to = {`/source/${item.value}`}
-                                className='sourceList'>
-                                {item.value}
-                            </Link>
-                        </li>
-                    })
-                }
-            </ul>
+                    color: '#e4e4e4',
+                    width: `${WIDTH * 121}px`,
+                    height: '41px',
+                    fontFamily: 'NotoSans',
+                    fontSize: '30px',
+                    fontWeight: 'bold',
+                    fontStretch: 'normal',
+                    fontStyle: 'normal',
+                    lineHeight: '1.37',
+                    letterSpacing: 'normal',
+                    textAlign: 'left',
+                    marginBottom: '33px'
+                }}>
+                Projects
+                </div>
+            <div>
+                <label
+                    style={{
+                        justifyContent: 'left',
+                        display: 'flex'
+                    }}>
+                    <input
+                        type='text'
+                        placeholder='Search...'
+                        style={{
+                            fontFamily: 'NotoSans',
+                            width: '361px',
+                            height: '44px',
+                            border: 'none',
+                            borderRadius: '9px',
+                            backgroundColor: '#f5f5f5',
+                            padding: '2px 8px',
+                            marginBottom: '52px'
+                        }}
+                        onChange={onChange} />
+                </label>
+            </div>
+            <div
+                style={{
+                    overflowY: 'scroll',
+                    height: '701px' // 추가 계산 필요
+                }}>
+                <ul
+                    style={{
+                        listStyleType: 'none',
+                        paddingLeft: '9px',
+                        fontFamily: 'NotoSans',
+                        fontSize: '20px',
+                        lineHeight: '34px',
+                        letterSpacing: '2px',
+                    }}>
+                    {
+                        Data.length === 0 ?
+                            OpenSourceData.map(item => {
+                                return <li key={item.id}>
+                                    <Link to={`/source/${item.value}`}
+                                        className='sourceList'>
+                                        {item.value}
+                                    </Link>
+                                </li>
+                            }) :
+
+                            Data.map(item => {
+                                return <li key={item.id} >
+                                    <Link
+                                        to={`/source/${item.value}`}
+                                        className='sourceList'>
+                                        {item.value}
+                                    </Link>
+                                </li>
+                            })
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
