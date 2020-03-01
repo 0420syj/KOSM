b1e98165b8dda419a059de9a3f5bb634d3e71fe7
0420syj <0420syj@naver.com>
2020-03-01 17:21:55 +0900

    게시판 완료

--- test6/frontend/src/board/Board.js
+++ test6/frontend/src/board/Board.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
-import paginationFactory, { PaginationProvider, PaginationTotalStandalone, PaginationListStandalone } from 'react-bootstrap-table2-paginator';
+import paginationFactory, { PaginationProvider, PaginationListStandalone } from 'react-bootstrap-table2-paginator';
 import { getBoards } from '../util/APIUtils'
 import BeforeMenu from '../menu/before/BeforeMenu';
 import AfterMenu from '../menu/after/AfterMenu';
@@ -23,28 +23,28 @@ const Board = (props) => {
 
     useEffect(() => {
         getBoards()
-        .then(response => {
-            setData(response);
-            console.log(response);
-        })
+            .then(response => {
+                setData(response);
+                console.log(response);
+            })
     }, []);
 
     const { SearchBar } = Search;
 
     const renderButton = () => {
-        if(localStorage.getItem('isLogin') !== 'false')
-        {
-            return (<div style={{float:'right', clear:'both'}}>
-            <Link to='/write'>
-                <Button className="button-write">작성하기</Button>
-            </Link>
-        </div>)
+        if (localStorage.getItem('isLogin') !== 'false') {
+            return (<div 
+                align="right"
+                >
+                <Link to='/write'>
+                    <Button className="button-write">작성하기</Button>
+                </Link>
+            </div>)
         }
     };
 
     const renderTopMenu = () => {
-        
-        if(localStorage.getItem('isLogin') !== 'false')
+        if (localStorage.getItem('isLogin') !== 'false')
             return <AfterMenu />
         else
             return <BeforeMenu />
@@ -59,8 +59,8 @@ const Board = (props) => {
                 padding: '0px',
             },
             headerStyle: () => {
-                return { 
-                    width: '70px' 
+                return {
+                    width: '70px'
                 };
             },
         },
@@ -69,15 +69,15 @@ const Board = (props) => {
             text: '제목',
             style: {
                 textAlign: 'left',
-                
+
                 paddingTop: '0px',
                 paddingBottom: '0px',
                 // paddingRight: '50px',
                 // paddingLeft: '50px',
             },
             headerStyle: () => {
-                return { 
-                    width: '200px', 
+                return {
+                    width: '200px',
                 };
             },
             formatter: (cell, row) => {
@@ -95,7 +95,7 @@ const Board = (props) => {
             headerStyle: () => {
                 return {
                     width: '90px',
-             };
+                };
             },
         },
         {
@@ -105,8 +105,8 @@ const Board = (props) => {
                 padding: '0px',
             },
             headerStyle: () => {
-                return { 
-                    width: '160px' 
+                return {
+                    width: '160px'
                 };
             },
         },
@@ -114,54 +114,64 @@ const Board = (props) => {
             dataField: 'createdDate',
             text: '등록일',
             type: 'date',
-             formatter: (cell) => {
-                 let dateObj = cell;
-                 if (typeof cell !== 'object') {
-                     dateObj = new Date(cell);
-                 }
+            formatter: (cell) => {
+                let dateObj = cell;
+                if (typeof cell !== 'object') {
+                    dateObj = new Date(cell);
+                }
                 return `${dateObj.getFullYear()}-${('0' + (dateObj.getMonth() + 1)).slice(-2)}-${('0' + dateObj.getDate()).slice(-2)}`;
-             },
-             style: {
+            },
+            style: {
                 padding: '0px',
             },
             headerStyle: () => {
-                return { 
-                    width: '160px' 
+                return {
+                    width: '160px'
                 };
             },
         }
     ];
 
-    const sizePerPageOptionRenderer = ({
-        text,
+    const pageButtonRenderer = ({
         page,
-        onSizePerPageChange
-      }) => (
-        <li
-          key={ text }
-          role="presentation"
-          className="dropdown-item"
-        >
-          <a
-            href="#"
-            tabIndex="-1"
-            role="menuitem"
-            data-page={ page }
-            onMouseDown={ (e) => {
-              e.preventDefault();
-              onSizePerPageChange(page);
-            } }
-            style={ { color: 'red' } }
-          >
-            { text }
-          </a>
-        </li>
-      );
-      
-      const options = {
-        sizePerPageOptionRenderer
+        active,
+        // disable,
+        // title,
+        onPageChange
+      }) => {
+        const handleClick = (e) => {
+          e.preventDefault();
+          onPageChange(page);
+        };
+        const activeStyle = {};
+        if (active) {
+          activeStyle.color = '#3aada8';
+          activeStyle.textDecoration = 'underline';
+        } else {
+          activeStyle.color = '#eaeaea';
+        }
+
+        if (typeof page === 'string') {
+          activeStyle.color = '#eaeaea';
+        }
+
+        return (
+          <li>
+              <a href="#" onClick={ handleClick } style={ activeStyle } className="page-button">{ page }</a>
+          </li>
+        );
       };
       
+      
+
+    const options = {
+        custom: true,
+        totalSize: data.length,
+        pageButtonRenderer,
+        prePageText: '◀',
+        nextPageText: '▶',
+    };
+
     return (
         <div className="container">
             <div className="top">
@@ -181,24 +191,43 @@ const Board = (props) => {
                             props => (
                                 <div>
                                     <h3 className="board-title">게시판</h3>
-                                    <div style={{float:'right', clear:'both'}}>
+                                    <div style={{ float: 'right', clear: 'both' }}>
                                         <SearchBar
-                                        {...props.searchProps}
-                                        className="search-field"
-                                        placeholder="Search..."
+                                            {...props.searchProps}
+                                            className="search-field"
+                                            placeholder="Search..."
                                         />
                                     </div>
-                                    <BootstrapTable
-                                        bordered={ false }
-                                        classes="table-borderless table-wrap"
-                                        headerWrapperClasses="table-head"
-                                        bodyClasses="table-body"
-                                        {...props.baseProps}
-                                        //pagination={paginationFactory()}
+                                    <PaginationProvider
                                         pagination={paginationFactory(options)}
-                                        noDataIndication="내용이 없습니다"
-                                    />
-                                    {renderButton()}
+                                    >
+                                        {
+                                            ({
+                                                paginationProps: {
+                                                    ...paginationProps
+                                                },
+                                                paginationTableProps
+                                            }) => (
+                                                    <>
+                                                        <BootstrapTable
+                                                            bordered={false}
+                                                            classes="table-borderless table-wrap"
+                                                            headerWrapperClasses="table-head"
+                                                            bodyClasses="table-body"
+                                                            {...props.baseProps}
+                                                            {...paginationTableProps}
+                                                            noDataIndication="내용이 없습니다"
+                                                        />
+                                                        {renderButton()}
+                                                        <div className="page-container">
+                                                            <PaginationListStandalone
+                                                                {...paginationProps}
+                                                        />
+                                                        </div>
+                                                    </>
+                                                )
+                                        }
+                                    </PaginationProvider>
                                 </div>
                             )
                         }
@@ -211,5 +240,7 @@ const Board = (props) => {
         </div>
     )
 }
+
+
 
 export default Board;
