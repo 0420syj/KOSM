2f6064b35f00e9a284971734de043e1f537f81b9
taewankang <king6096211@naver.com>
2020-03-01 22:06:53 +0900

    비밀번호 변경 페이지 완료
    비밀번호 변경 페이지 완료

--- test6/frontend/src/App.js
+++ test6/frontend/src/App.js
@@ -6,6 +6,7 @@ import Write from './board/Write';
 import Article from './board/Article'
 import Source from './source/Source';
 import MyPage from './mypage/MyPage';
+import ChangePassword from './sign/login/ChangePassword';
 import ForgotPassword from './sign/login/ForgotPassword';
 import Login from './sign/login/Login';
 import DeleteUser from './mypage/DeleteUser';
@@ -28,132 +29,10 @@ function App() {
         <Route exact path='/mypage' component={MyPage}/>
         <Route exact path='/forgot' component={ForgotPassword}/>
         <Route exact path='/deleteuser' component={DeleteUser}/>
+        <Route exact path='/changepwd' component={ChangePassword}/>
       </Router>
     </div>
   );
 }
 
 export default App;
-
-// import React, {Component} from 'react';
-
-// import {withRouter, Switch, Route} from 'react-router-dom';
-// import { getCurrentUser } from './util/APIUtils';
-// import { ACCESS_TOKEN } from './constants';
-
-// import SignUp from './signup/SignUp';
-// import Login from './login/Login';
-
-// import AppHeader from './common/AppHeader';
-// import LoadingIndicator from './common/LoadingIndicator';
-// import Profile from './profile/Profile';
-
-// import { Layout, notification } from 'antd';
-// const { Content } = Layout;
-
-
-// // import { render } from 'node-sass';
-// // import { LayoutContext } from 'antd/lib/layout/layout';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       currentUser: null,
-//       isAuthenticated: false,
-//       isLoading: false,
-//       role: null,
-//       username : null
-//     }
-//     this.handleLogout = this.handleLogout.bind(this);
-//     this.loadCurrentUser = this.loadCurrentUser.bind(this);
-//     this.handleLogin = this.handleLogin.bind(this);
-
-//     notification.config({
-//       placement: 'topRight',
-//       top: 70,
-//       duration: 3,
-//   });  
-//   }
-
-//   loadCurrentUser() {
-//     this.setState({
-//       isLoading: true
-//     });
-//     getCurrentUser()
-//     .then(response => {
-//       this.setState({
-//         currentUser: response,
-//         isAuthenticated: true,
-//         isLoading: false,
-//         username : response.username,
-//         role : response.authorities[0].authority
-//       },function(){
-//       });
-//     }).catch(error => {
-//       this.setState({
-//         isLoading: false
-//       });  
-//     });
-//   }
-
-//   componentDidMount() {
-//     this.loadCurrentUser();
-//   }
-
-//   // Handle Logout, Set currentUser and isAuthenticated state which will be passed to other components
-//   handleLogout(redirectTo="/", notificationType="success", description="You're successfully logged out.") {
-//     localStorage.removeItem(ACCESS_TOKEN);
-
-//     this.setState({
-//       currentUser: null,
-//       isAuthenticated: false,
-//       role:null,
-//       username: null
-//     });
-
-//     this.props.history.push(redirectTo);
-//     notification[notificationType]({
-//       message: 'Cheeze Toon',
-//       description: description,
-//     });
-//   }
-
-//   handleLogin() {
-//     notification.success({
-//       message: 'Cheeze Toon',
-//       description: "로그인 되었습니다.",
-//     });
-//     this.loadCurrentUser();
-//     this.props.history.push("/");
-//   }
-
-//   render(){
-//     if(this.state.isLoading) {
-//       return <LoadingIndicator />
-//     }
-//     return (
-//       <Layout className="app-container">
-//       <AppHeader isAuthenticated={this.state.isAuthenticated} 
-//         currentUser={this.state.currentUser} 
-//         onLogout={this.handleLogout} 
-//         role={this.state.role}/>
-
-//       <Content className="app-content">
-//         <div className="container">
-//           <Switch>      
-           
-//             <Route exact path="/login" 
-//               render={(props) => <Login onLogin={this.handleLogin} {...props} />}></Route>
-//             <Route exact path='/signup' component={SignUp}/>
-//             <Route path="/users/:username" 
-//               render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}/>
-            
-//           </Switch>
-//         </div>
-//       </Content>
-//       </Layout>
-//     );
-//   }
-// }
-// export default withRouter(App);
---
+++ test6/frontend/src/sign/login/ChangePassword.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './ChangePassword.scss';
+const ForgotPassword = () => {
+    return ( 
+        <div className='forgotContainer'>
+            <div className='title'>
+                <div className='title1'>Join KOSM</div>
+                <div className='title2'>Change Password</div>
+            </div>
+            <div className='mainContents'>
+                <div className='contents'>
+                    <div className='subContents'>
+                        <div className='subTitles'>기존 비밀번호</div>
+                        <div className='inputContents'>
+                            <input className='inputs'/>
+                        </div>
+                    </div>
+                    <div style={{marginTop: '21px'}} className='subContents'>
+                        <div className='subTitles'>새 비밀번호</div>
+                        <div className='inputContents'>
+                            <input className='inputs'/>
+                        </div>
+                    </div>
+                    <div className='subContents'>
+                        <div className='subTitles'>비밀번호 확인</div>
+                        <div className='inputContents'>
+                            <input className='inputs'/>
+                        </div>
+                    </div>                
+                    <button className='changeButton'>비밀번호 변경</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ForgotPassword;
