635b1fc4623eb6f15d727f0ee068334f89d92910
taewankang <king6096211@naver.com>
2020-03-02 20:41:55 +0900

    Sign up 수정
    Sign up 수정

--- test6/frontend/src/App.js
+++ test6/frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route exact path='/login' component={Login}/>
         <Route exact path='/signup' component={SignUp}/>
         <Route exact path='/board' component={Board}/>
-        <Route exact path='/success/:id' component={success}/>
+        <Route exact path='/success' component={success}/>
         <Route exact path='/write' component={Write}/>
         <Route exact path='/article/:email' component={Article}/>
         <Route exact path='/source/:item' component={Source}/>
