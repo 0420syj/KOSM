ebd12b17975c9814447080c661480a92a01a3c7d
taewankang <king6096211@naver.com>
2020-03-22 15:58:24 +0900

    로딩중 기능 추가
    로딩중 기능 추가

--- test6/frontend/src/source/detail/DetailMain.js
+++ test6/frontend/src/source/detail/DetailMain.js
@@ -1,12 +1,24 @@
 /*
     버전별 세부사항 메인 페이지
 */
-import React from 'react';
+import React, {useEffect} from 'react';
 import SourceList from '../../menu/SourceList';
 import DetailTitle from './DetailTitle';
 import DetailContents from './DetailContents';
 import './DetailMain.scss';
+import {detailCrawl} from '../../util/APIUtils';
 const DetailMain = ({match}) => {
+    const url = "https://nvd.nist.gov/vuln/detail/" + match.params.source;
+    console.log(match.params.source);
+    useEffect(() => {
+        const signupRequest = {url:url}
+        detailCrawl(signupRequest)      
+            .then(res => {
+                console.log(res);       //여기서 데이터 불러 옴
+            })
+            .catch(e => console.log(e));
+    });
+
     return ( 
         <div>
             <DetailTitle/>
--- test6/frontend/src/source/main_code/versions_after_login_title/AfterMainSource.js
+++ test6/frontend/src/source/main_code/versions_after_login_title/AfterMainSource.js
@@ -8,21 +8,6 @@ import './AfterMainSource.scss';
 const AfterMainSource = ({isFavorite, favoriteClick, name, title, date}) => {
     return ( 
         <div className='beforeMainSource'>
-            <div className='topStar'>
-                <div className='star'>
-                    {
-                        isFavorite === true ?
-                        <IconContext.Provider
-                            value={{color: 'yellow', size: '30px'}}>
-                            <MdStar onClick = {favoriteClick}/> 
-                        </IconContext.Provider> :
-                        <MdStarBorder size='30px' onClick = {favoriteClick}/>
-                    }
-                </div>
-                <div className='topName'>
-                    {name}
-                </div>
-            </div>
             <span className='topLicense'>
                 {title}
             </span>
