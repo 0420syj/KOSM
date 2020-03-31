2d4fd1b02f3f128ddf881b6a49c8174af9f02cfb
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-04-01 01:08:16 +0900

    ss
    ss

--- test6/frontend/src/source/main_code/versions_after_login_title/AfterMainSource.js
+++ test6/frontend/src/source/main_code/versions_after_login_title/AfterMainSource.js
@@ -3,38 +3,72 @@
 */
 import React, {useState, useEffect} from 'react';
 import './AfterMainSource.scss';
-const AfterMainSource = ({title, github, date, graph}) => {
+const AfterMainSource = ({release, releaseDate, github, date, graph}) => {
     const [newDate, setNewDate] = useState([]);
     console.log(github);
     useEffect(() => {
         setNewDate(date.split(';'))
     }, [])
     return ( 
-        <div className='beforeMainSource'>
-            <span className='topLicense'>
-                {title}
-            </span>
-            <span className='topPage'>
+        <div className='afterMainSource'>
+            <div className='div'>
+                <span className='topPage'>
+                    {
+                        graph !== null&&
+                        <span className='link'>
+                            <a      //그래프 사이트로 가는 링크
+                                className='link-text' 
+                                href={graph}>
+                                link to Vulnerability statistics
+                            </a>
+                        </span>
+                    }
+                    {
+                        github !== null &&
+                        <span className='link'>
+                            <a      //깃허브 사이트로 가는 링크
+                                className='link-text'
+                                href={github}>
+                                link to Github site
+                            </a>
+                        </span>
+                    }
+                </span>
+            </div>
+            <div className='topPage' style={{paddingBottom: '20px'}}>
+                <span className='text'>
+                    <span>
+                        <span className='title'>
+                            CVE Last modified: &nbsp;
+                        </span>
+                        <span>      
+                            {newDate[0]}     
+                        </span>
+                    </span>
+                </span>
                 {
-                    github !== null &&
-                    <a
-                        className='link' 
-                        href={github}>
-                        Github
-                    </a>
+                    releaseDate !== null &&
+                    <span className='text' style={{width: '260px'}}>
+                        <span className='title'>
+                            latest version date: &nbsp;
+                        </span>
+                        <span>
+                            {releaseDate}
+                        </span>
+                    </span>
                 }
                 {
-                    graph !== null&&
-                    <a
-                        className='link'
-                        href={graph}>
-                        graph
-                    </a>
+                    release !== null &&
+                    <span className='text' style={{width: '400px'}}>
+                        <span className='title'>
+                            Latest version: &nbsp;
+                        </span>
+                        <span>
+                            {release}
+                        </span>
+                    </span>
                 }
-            </span>
-            <span className='topDate'>
-                {newDate[0]}
-            </span>
+            </div>
         </div>
     )
 }
