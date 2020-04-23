03a3c3b18f7a51042a89667f89fb90c9e264b408
0420syj <0420syj@naver.com>
2020-04-24 03:04:36 +0900

    디테일 페이지 점수
    점수 출력 CSS 수정

--- test6/frontend/src/source/detail/DetailContents.js
+++ test6/frontend/src/source/detail/DetailContents.js
@@ -6,13 +6,57 @@ import './DetailContents.scss';
 const DetailContents = ({infos, link, title, scores}) => {
     const [linkArray, setLink] = useState([]);
     let idx = 0;
-    useEffect(() => {setLink(link.split(','));}, [link])
+    useEffect(() => {
+        setLink(link.split(','));
+    }, [link])
+
+    let arr = scores.split(',');
+    let scoreV3, scoreV2;
+    if(arr.length === 2){
+        scoreV3 = arr[1]
+        scoreV2 = arr[0]
+    }
+        
+    else{
+        scoreV3 = arr[0]
+        scoreV2 = arr[1]
+    }
+        
+    // console.log(arr)
+
+    const colorRender = data => {
+        let flag
+        if(data === '' || arr.length === 1) flag = data
+        else flag = data.substring(0,2)
+        switch (flag) {
+            case '0.':
+                return <span style={{ marginLeft: '25px', height: '30px', background: 'darkgrey', color: 'black', fontWeight: 'bold' }}>&nbsp;N/A&nbsp;<br /><br /></span>
+            case '1.':
+            case '2.':
+            case '3.':
+            case '4.':
+                return <span style={{ marginLeft: '25px', height: '30px', background: '#F2CC0C', color: 'black', fontWeight: 'bold' }}>&nbsp;{data}&nbsp;<br /><br /></span>
+            case '5.':
+            case '6.':
+                return <span style={{ marginLeft: '25px', height: '30px', background: '#EC971F', color: 'black', fontWeight: 'bold' }}>&nbsp;{data}&nbsp;<br /><br /></span>
+            case '7.':
+            case '8.':
+                return <span style={{ marginLeft: '25px', height: '30px', background: '#d9534f', color: 'black', fontWeight: 'bold' }}>&nbsp;{data}&nbsp;<br /><br /></span>
+            case '9.':
+                return <span style={{ marginLeft: '25px', height: '30px', background: '#000000', color: '#a9a9a9', fontWeight: 'bold' }}>&nbsp;{data}&nbsp;<br /><br /></span>
+            case '10':
+                return <span style={{ marginLeft: '25px', height: '30px', background: '#000000', color: '#a9a9a9', fontWeight: 'bold' }}>&nbsp;{data}&nbsp;<br /><br /></span>
+            default:
+                return <span style={{ marginLeft: '25px', height: '30px', background: 'darkgrey', color: 'black', fontWeight: 'bold' }}>&nbsp;N/A&nbsp;<br /><br /></span>
+        }
+    }
 
     return ( 
         <div className='detailContents'>
             <div style={{marginTop: '30px'}}></div>
             <div className='scores'>
-                {scores}
+            <span style={{display: 'inline-block', width: '35px'}}>V3:</span>{colorRender(scoreV3)}
+            <span style={{display: 'inline-block', width: '35px'}}>V2.1:</span>{colorRender(scoreV2)}
             </div>
             <div className='date'>
                 <span className='lastModified'>
